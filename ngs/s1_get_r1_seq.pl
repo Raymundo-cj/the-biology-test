529ba2dfebff52b63be15c8fa37c98bd3080377f
风从远方吹来 <64938817+Raymundo-cj@users.noreply.github.com>
2023-08-29 20:57:52 +0800

    Create s1_get_r1_seq.pl

---
+++ ngs/s1_get_r1_seq.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -w
+use strict;
+open F, $ARGV[0] or die;
+open R1,"zcat $ARGV[1]|" or die;
+my %hash;
+
+while(<F>){
+	my @ss = split;
+	for (my $i = 1;$i < @ss;$i++){
+		$hash{$ss[$i]} = $ss[0];
+	}
+}
+
+while(<R1>){
+	chomp;
+	my @ss=split/\s/,$_;
+	my $header=$ss[0];
+    chomp(my $sequence=<R1>);
+    chomp(my $comment=<R1>);
+    chomp(my $quality=<R1>);
+	if (exists $hash{$header}){
+		open NEW, ">>./all_library/$hash{$header}_R1.fastq";
+		print NEW "$_\n$sequence\n$comment\n$quality\n";
+	}
+}
